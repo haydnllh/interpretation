3db4a4a2135708da90978f366e8adeffc9d8f9dc
haydnllh <haydnllh719@gmail.com>
2026-08-10 14:59:07 +0100

    Added nn to test configs

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,9 @@ import pytest
 from sklearn.datasets import make_regression, make_classification
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+import torch.nn as nn
+import keras
+from keras import layers
 
 
 @pytest.fixture(scope="session")
@@ -55,3 +58,58 @@ def multiclass_classifier():
     model.fit(X, y)
 
     return model, X, y
+
+def torch_classifier(n_input, n_output):
+    class NNClassifier(nn.Module):
+        def __init__(self, n_input, n_output):
+            super().__init__()
+
+            self.network = nn.Sequential(
+                nn.Linear(n_input, n_input * 2),
+                nn.ReLU(),
+                nn.Linear(n_input * 2, n_output),
+                nn.Softmax(dim=1)
+            )
+
+        def forward(self, x):
+            return self.network(x)
+    
+    return NNClassifier(n_input, n_output)
+
+def torch_regressor(n_input, n_output):
+    class NNRegressor(nn.Module):
+        def __init__(self, n_input, n_output):
+            super().__init__()
+
+            self.network = nn.Sequential(
+                nn.Linear(n_input, n_input * 2),
+                nn.ReLU(),
+                nn.Linear(n_input * 2, n_output),
+            )
+
+        def forward(self, x):
+            return self.network(x)
+        
+    return NNRegressor(n_input, n_output)
+
+
+    
+def tf_classifier(n_input, n_output):
+    model = keras.Sequential(
+        [
+            layers.Input(shape=(n_input,)),
+            layers.Dense(n_input * 2, activation="relu"),
+            layers.Dense(n_output, activation="softmax")
+        ]
+    )
+    return model
+    
+def tf_regressor(n_input, n_output):
+    model = keras.Sequential(
+        [
+            layers.Input(shape=(n_input,)),
+            layers.Dense(n_input * 2, activation="relu"),
+            layers.Dense(n_output)
+        ]
+    )
+    return model
