3baddbd0b3613a4c31a2ede6620c8a7481e82b9b
haydnllh <haydnllh719@gmail.com>
2026-08-26 17:12:57 +0100

    Test for individual neuron derivatives

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -19,14 +19,40 @@ def test_torch_gradient_numbers():
     X = torch.tensor([[1]], dtype=torch.float32)
     fn = lambda x : x.mean()
     
-    grad1 = model.compute_gradients(X, objective_layer=1, objective_fn=fn).item()
-    grad2 = model.compute_gradients(X, objective_layer=0, objective_fn=fn).item()
-    grad3 = model.compute_gradients(X, objective_layer="1", objective_fn=fn).item()
-    grad4 = model.compute_gradients(X, objective_layer="0", objective_fn=fn).item()
-    grad5 = model.compute_gradients(X, objective_layer=1, objective_fn=fn, wrt_layer=0)
+    grad1 = model.compute_gradients(X, objective=1, objective_fn=fn).item()
+    grad2 = model.compute_gradients(X, objective=0, objective_fn=fn).item()
+    grad3 = model.compute_gradients(X, objective="1", objective_fn=fn).item()
+    grad4 = model.compute_gradients(X, objective="0", objective_fn=fn).item()
+    grad5 = model.compute_gradients(X, objective=1, objective_fn=fn, wrt=0)
     
     assert grad1 == 8.0
     assert grad2 == 1.5
     assert grad3 == 8.0
     assert grad4 == 1.5
     assert (grad5 == np.array([[2.0, 3.0]])).all()
+    
+def test_torch_neuron_gradient_numbers():
+    torch_model = nn.Sequential(
+        nn.Linear(1,2),
+        nn.Linear(2,2)
+    )
+    torch_model[0].weight = nn.Parameter(torch.tensor([[1],[2]], dtype=torch.float32))
+    torch_model[1].weight = nn.Parameter(torch.tensor([[2,3],[1,2]], dtype=torch.float32))
+    model = wrap_model(torch_model)
+    
+    X = torch.tensor([[1]], dtype=torch.float32)
+    fn = lambda x : x.mean()
+    
+    grad1 = model.compute_gradients(X, objective=(1,0), objective_fn=fn, wrt=(0,0)).item()
+    grad2 = model.compute_gradients(X, objective=(1,1), objective_fn=fn, wrt=(0,0)).item()
+    grad3 = model.compute_gradients(X, objective=(1,0), objective_fn=fn, wrt=(0,1)).item()
+    grad4 = model.compute_gradients(X, objective=(1,1), objective_fn=fn, wrt=(0,1)).item()
+    grad5 = model.compute_gradients(X, objective=(1,0), objective_fn=fn).item()
+    grad6 = model.compute_gradients(X, objective=(1,1), objective_fn=fn).item()
+    
+    assert grad1 == 2.0
+    assert grad2 == 1.0
+    assert grad3 == 3.0
+    assert grad4 == 2.0
+    assert grad5 == 8.0
+    assert grad6 == 5.0
