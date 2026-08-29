fd8dc700cc0645371ad155fd59994d37e99ff126
haydnllh <haydnllh719@gmail.com>
2026-08-29 16:45:36 +0100

    Added plotting saliency map

--- interpretation/explainer/nn/saliency/explainer.py
+++ interpretation/explainer/nn/saliency/explainer.py
@@ -2,6 +2,9 @@ import numpy as np
 import numpy.typing as npt
 import torch
 import tensorflow as tf
+import matplotlib.pyplot as plt
+from matplotlib.axis import Axis
+from matplotlib.figure import Figure
 
 from ..nn_explainer import NNExplainer
 from ....utils.validate_input import validate_input_1d
@@ -38,16 +41,18 @@ class SaliencyMap(NNExplainer):
         Parameters
         ----------
         X : npt.NDArray
-            Instance to produce the saliency map on.
+            Instance to produce the saliency map on
         class_idx : int
-            The objective class of the saliency map.
+            The objective class of the saliency map
         method : str, optional
-            Algorithm used for computing the saliency map, by default "vanilla"
+            Algorithm used for computing the saliency map, by default "SmoothGrad"
+        n_samples : int, optional
+            Number of samples to average from when using "SmoothGrad", by default 50
 
         Returns
         -------
-        _type_
-            _description_
+        npt.NDArray
+            Resulting saliency map, the partial derivative of class score w.r.t. input, shape is the same as the input
         """
         if self.istorch:
             objective_fn = lambda x : x.mean()
@@ -61,7 +66,6 @@ class SaliencyMap(NNExplainer):
                 objective_fn=objective_fn
             )
             
-            return grad
         elif method == "SmoothGrad":
             grad = smooth_grad(
                 self.model,
@@ -71,6 +75,55 @@ class SaliencyMap(NNExplainer):
                 n_samples=n_samples
             )
             
-            return grad
         else:
             raise ValueError(f"Method {method} is not supported, only valid methods are 'vanilla' and 'SmoothGrad'.")
+        
+        return grad
+    
+    def plot_map(
+        self,
+        X: npt.NDArray,
+        class_idx: int,
+        method: str = "SmoothGrad",
+        n_samples: int = 50,
+        ax: Axis | None = None
+    ):
+        """Plots the saliency map of the input on an axis.
+
+        Parameters
+        ----------
+        X : npt.NDArray
+            Instance to produce the saliency map on
+        class_idx : int
+            The objective class of the saliency map
+        method : str, optional
+            Algorithm used for computing the saliency map, by default "SmoothGrad"
+        n_samples : int, optional
+            Number of samples to average from when using "SmoothGrad", by default 50
+        ax : Axis | None, optional
+            Matplotlib axis to plot on, by default None
+
+        Returns
+        -------
+        fig : Figure
+            Matplotlib Figure object containing the saliency map axis
+        ax : Axis
+            Matplotlib Axis object that the saliency map is plotted on
+        """
+        map = self.compute_map(X, class_idx, method, n_samples)
+        
+        map = np.transpose(map.squeeze(), (1,2,0))
+        map = np.max(np.abs(map), axis=-1)
+        map = (map - np.min(map)) / (np.max(map) - np.min(map) + 1e-8)
+        
+        if ax is None:
+            fig, ax = plt.subplots(figsize=(8, 8))
+        else:
+            fig = ax.figure
+            
+        img = ax.imshow(map, cmap="jet")
+        ax.axis("off")
+        
+        fig.colorbar(img, ax=ax)
+        
+        return fig, ax
